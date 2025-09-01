1a98d242d236c31f03bb6b6e793d2176e9c6def7
Thawatchai Rueanin <thawatchairueanin5@gmail.com>
2025-09-01 20:50:53 +0700

    s

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import history from "connect-history-api-fallback";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    middlewareMode: true,
-    setupMiddlewares: (middlewares) => {
-      middlewares.use(history());
-      return middlewares;
-    },
-  },
 });
