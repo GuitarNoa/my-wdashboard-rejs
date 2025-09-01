08731cddefdf41b938b70d03bef1f419b033956e
Thawatchai Rueanin <thawatchairueanin5@gmail.com>
2025-09-01 20:46:22 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // fallback ทุก route ไปที่ index.html สำหรับ SPA
+    historyApiFallback: true, // Vite 4+ บางเวอร์ชันอาจต้องติดตั้ง connect-history-api-fallback
+  },
 });
